b9234ba1891f877316c0a1795c5e2b0a3a91c45b
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-25 16:01:53 +0530

    Created ASP.NET Core new Project and included .gitignore file

---
+++ Training.NETCoreMVC.DrinkAndGo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Training.NETCoreMVC.DrinkAndGo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

c79b05c90f46fc1a15e5fb927dfa52d60ea385b7
moh-faisal85 <moh.faisal85@gmail.com>
2025-01-25 16:21:25 +0530

    Create a class for Category and Drink Model

---
+++ Training.NETCoreMVC.DrinkAndGo/Data/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Training.NETCoreMVC.DrinkAndGo.Data.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+        public List<Drink> Drinks { get; set; }
+    }
+}
---
+++ Training.NETCoreMVC.DrinkAndGo/Data/Models/Drink.cs
@@ -0,0 +1,19 @@
+﻿namespace Training.NETCoreMVC.DrinkAndGo.Data.Models
+{
+   
+    public class Drink
+    {
+        public int DrinkId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public string LongDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsPreferredDrink { get; set; }
+        public int IntStock { get; set; }
+        public int CategoryId {  get; set; }
+        public virtual Category Category { get; set; }
+
+    }
+}

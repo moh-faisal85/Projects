4f1537a236198414add068f5b6ef433bf3557e1e
moh-faisal85 <moh.faisal85@gmail.com>
2025-11-05 01:14:34 +0530

    Section:4.3 - Automapper Integration. - Mapping when number of Properties and its data types are  match with source and Target Object

---
+++ Training.DotNetCore.Project/Training.DotNetCore.Project.API/Mappings/AutoMapperProfiles.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+
+namespace Training.DotNetCore.Project.API.Mappings
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            //DTO to Domain Model Mapping when number of Properties and its types match with source and Target Object
+            CreateMap<UserDTO, UserDomainModel>();
+
+            //Domain Model to DTO Mapping or Reverse Mapping
+            //CreateMap<UserDomainModel, UserDTO>();
+            CreateMap<UserDTO, UserDomainModel>().ReverseMap();
+
+
+        }
+        public class UserDTO
+        {
+            public string FullName { get; set; }
+        }
+        public class UserDomainModel
+        {
+            public string FullName { get; set; }
+        }
+    }
+}
